25395cc0f3290bf6d710cfea411a958b09f56ce8
Andrew Wood <andrew.wood@.ed.ac.uk>
2021-06-21 13:14:59 +0100

    Remove qgis path requirement

--- src/gps_reader_pkg/qgis_app_init.py
+++ src/gps_reader_pkg/qgis_app_init.py
@@ -1,21 +1,26 @@
 from qgis.core import (QgsApplication)
 from pathlib import Path
 
+
 class QgisSetup:
 
     def __init__(self):
-        pass
-
-    def setup(self, config):
-
-        qgis_path = config['qgis_path']
-        if qgis_path == None:
-            print('QGIS location not given')
-            return False
-        if not (Path(qgis_path).exists()):
-            print('QGIS file not found')
-            return False
-        QgsApplication.setPrefixPath(qgis_path, True)
         self.qgs = QgsApplication([], False)
         self.qgs.initQgis()
-        return self.qgs
+
+    def exit(self):
+        self.qgs.exit()
+
+    # def setup(self, config):
+
+    # qgis_path = config['qgis_path']
+    # if qgis_path == None:
+    #     print('QGIS location not given')
+    #     return False
+    # if not (Path(qgis_path).exists()):
+    #     print('QGIS file not found')
+    #     return False
+    # QgsApplication.setPrefixPath(qgis_path, True)
+    # self.qgs = QgsApplication([], False)
+    # self.qgs.initQgis()
+    # return self.qgs
